4953f29fabe52acff3d8af1939e791dd2acf4a36
25JdS01-Josedeleon <jleondb25@gmail.com>
2026-03-24 09:51:06 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("====Promedio de calificaciones====");
+        string materia;
+        int n1, n2, n3, n4, n5;
+        double promedio;
+        Console.WriteLine("Ingrese el nombre de la materia: ");
+        materia = Console.ReadLine();
+        Console.WriteLine("dame 5 notas: ");
+        n1 = Convert.ToInt32(Console.ReadLine());
+        n2 = Convert.ToInt32(Console.ReadLine());
+        n3 = Convert.ToInt32(Console.ReadLine());
+        n4 = Convert.ToInt32(Console.ReadLine());
+        n5 = Convert.ToInt32(Console.ReadLine());
+
+        promedio = (n1 + n2 + n3 + n4 + n5) / 5;
+
+        if (promedio >= 70)
+            Console.WriteLine("El promedio es: "+promedio+ " y el estudiante aprobo");
+        else
+            Console.WriteLine("El promedio es: "+promedio+" y el estudiante resaprobo");
+        {
+
+        }
+    }
+}
