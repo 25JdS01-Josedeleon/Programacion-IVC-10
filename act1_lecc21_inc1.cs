4ab7a0eff7a1fe56a2be2a4384bb90f063742cbe
25JdS01-Josedeleon <jleondb25@gmail.com>
2026-04-04 14:03:59 -0500

    Add files via upload

---
+++ act1_lecc21_inc1.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("==DIA DE LA SEMANA==");
+        int dia;
+        Console.Write("Dime un número del 1 al 7: ");
+        dia = Convert.ToInt32(Console.ReadLine());
+        if (dia == 1)
+            Console.WriteLine("Hoy es Lunes");
+        else if (dia == 2)
+            Console.WriteLine("Hoy es Martes");
+        else if (dia == 3)
+            Console.WriteLine("Hoy es Miércoles");
+        else if (dia == 4)
+            Console.WriteLine("Hoy es Jueves");
+        else if (dia == 5)
+            Console.WriteLine("Hoy es Viernes");
+        else if (dia == 6)
+            Console.WriteLine("Hoy es Sabado");
+        else if (dia == 7)
+            Console.WriteLine("Hoy es Domingo");
+        else
+            Console.WriteLine("NUMERO INVALIDO");
+    }
+}
